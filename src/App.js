c96467b4b900ce8518744e289f8bc59bf4374c36
thedreamJK7 <jkubaev7@gmail.com>
2022-06-14 22:05:48 +0500

    14-iyun

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenseitem = [
+    {
+      id: "e1",
+      title: "New Desk(Wooden)",
+      amount: 450,
+      date: new Date(2022, 10, 4),
+    },
+    {
+      id: "e2",
+      title: "Lamborghini",
+      amount: 450000,
+      date: new Date(2022, 5, 4),
+    },
+    {
+      id: "e3",
+      title: "Iphone 14 pro",
+      amount: 2300,
+      date: new Date(2022, 11, 12),
+    },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ExpenseItem
+        id={expenseitem[0].id}
+        title={expenseitem[0].title}
+        amount={expenseitem[0].amount}
+        date={expenseitem[0].date}
+      />
+      <ExpenseItem
+        id={expenseitem[1].id}
+        title={expenseitem[1].title}
+        amount={expenseitem[1].amount}
+        date={expenseitem[1].date}
+      />
+      <ExpenseItem
+        id={expenseitem[2].id}
+        title={expenseitem[2].title}
+        amount={expenseitem[2].amount}
+        date={expenseitem[2].date}
+      />
     </div>
   );
 }
