b57ba2ac66bf186c0d7a00427abcefc1ebfc709b
thedreamJK7 <jkubaev7@gmail.com>
2022-07-05 20:29:17 +0500

    Project finished

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -7,7 +7,6 @@ const ChartBar = (props) => {
     barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
   }
 
-  console.log(barFillHeight);
   return (
     <div className="chart-bar">
       <div className="chart-bar__inner">
